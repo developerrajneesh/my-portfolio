00bf8d7753541a5d66f0c368ed5e16e29c908fff
Rajneesh Shukla <124131361+developerrajneesh@users.noreply.github.com>
2023-07-16 18:22:54 +0530

    Delete src directory

--- src/assets/index.jsx
+++
@@ -1,7 +0,0 @@
-import HeroImg from './hero.png';
-import AboutImg from './aboutImg.png';
-import JobportalImg from './jobportalImg.png';
-import SlickdealsImg from './SlickdealsImg.png';
-import JusnestImg from './jusnestImg.png';
-import HepptogoImg from './hepptogo.png';
-export {HeroImg,SlickdealsImg,JusnestImg,HepptogoImg,JobportalImg,AboutImg}
