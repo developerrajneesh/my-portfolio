28517a933bff3afb59094c4b84e19982674a6f06
Rajneesh Shukla <124131361+developerrajneesh@users.noreply.github.com>
2023-07-16 16:35:30 +0530

    Add files via upload

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+function Navbar({handleScroll,Ref1,Ref2,Ref3,Ref4,Ref5,Ref6,Ref7}) {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg  bg-dark  shadow fixed-top">
+        <div className="container-fluid">
+          <span className="navbar-brand text-white ms-lg-5  cursor-pointer"  onClick={()=>handleScroll(Ref1)}>
+            Rajneesh
+          </span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+            <form className="d-flex" role="search">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 me-lg-5">
+                <li className="nav-item" onClick={()=>handleScroll(Ref1)}>
+                  <span
+
+                    className="nav-link text-light"
+                    aria-current="page"
+                  >
+                    Home
+                  </span>
+                </li>
+                <li className="nav-item" onClick={()=>handleScroll(Ref2)}>
+                  <span
+                    className="nav-link text-light"
+                    aria-current="page"
+                    
+                  >
+                    About
+                  </span>
+                </li>
+                <li className="nav-item" onClick={()=>handleScroll(Ref3)}>
+                  <span
+                    className="nav-link text-light"
+                    aria-current="page"
+                   
+                  >
+                    Resume
+                  </span>
+                </li>
+                <li className="nav-item"onClick={()=>handleScroll(Ref4)}>
+                  <span
+                    className="nav-link text-light"
+                    aria-current="page"
+                   
+                  >
+                    Services
+                  </span>
+                </li>
+                <li className="nav-item"onClick={()=>handleScroll(Ref5)}>
+                  <span
+                    className="nav-link text-light"
+                    aria-current="page"
+                  >
+                    Skills
+                  </span>
+                </li>
+                <li className="nav-item" onClick={()=>handleScroll(Ref6)}>
+                  <span
+                    className="nav-link text-light"
+                    aria-current="page"
+                  >
+                    Projects
+                  </span>
+                </li>
+                <li className="nav-item" onClick={()=>handleScroll(Ref7)}>
+                  <span
+                    className="nav-link text-light"
+                    aria-current="page"
+                  >
+                    Contact
+                  </span>
+                </li>
+                {/* <li className="nav-item">
+                  <a
+                    className="nav-link text-light"
+                    aria-current="page"
+                    href="#"
+                  >
+                    My Blog
+                  </a>
+                </li> */}
+              </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import "./skills.css";
+function Skills() {
+  return (
+    <div className="margin-top">
+      <div className="mt-5 mt-3 d-flex justify-content-center">
+        <div className="position-relative">
+          <h1 className="heading-resume">My Skills</h1>
+          <h1 className=" about-text">SKILLS</h1>
+        </div>
+      </div>
+
+      <p className="mt-5 text-center">
+        Far far away, behind the word mountains, far from the countries Vokalia
+        and Consonantia
+      </p>
+
+      <div className="row m-0">
+        <div className="col-12 col-md-6 mt-4">
+          <h4>HTML</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>CSS</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>BOOTSTRAP</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>MATERIAL UI</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>TAILWIND</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>JAVA SCRIPT</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>TYPE SCRIPT</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>REACT JS</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>ANGULAR</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>NEXT JS</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>REACT NATIVE</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-4">
+          <h4>NODE JS</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 mt-4">
+          <h4>EXPRESS JS</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 mt-4">
+          <h4>MONGO DB</h4>
+          <div className="progress">
+            <div class="progress-bar color-1" style={{ width: "90%" }}>
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
